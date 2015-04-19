1044fcd9922387cc69849b9b23a2a3e7368ca7c6
Ivan Kryvoruchko <gildraug@gmail.com>
2015-04-19 21:56:00 +0300

    Initial attempt to make map with dynamic color change

--- src/cljs/loki/app.cljs
+++ src/cljs/loki/app.cljs
@@ -12,7 +12,9 @@
             [cljsjs.leaflet]))
 
 (defonce initial-state
-  {:geojson nil})
+  {:geojson nil
+   :leaflet-geojson nil
+   :regions-data {}})
 
 
 (register-sub
@@ -20,6 +22,18 @@
  (fn
    [db _]
    (reaction (:geojson @db))))
+
+(register-sub
+ :regions-data
+ (fn
+   [db _]
+   (reaction (:regions-data @db))))
+
+(register-sub
+ :leaflet-geojson
+ (fn
+   [db _]
+   (reaction (:leaflet-geojson @db))))
 
 
 (defn event-type
@@ -30,11 +44,20 @@
 
 (def GeoJSON js/Object)
 
+(def LeafletGeoJson js/Object)
+
 (def GeoJSONReceivedEvent
   (event-type :geojson-received (s/one GeoJSON "geojson")))
 
 (def InitializeEvent
   (event-type :initialize))
+
+(def RegionDataReceivedEvent
+  (event-type :region-data-received
+              (s/one s/Str "region") {:wins s/Int :treasons s/Int}))
+
+(def LeafletGeoJsonAddedEvent
+  (event-type :leaflet-geojson-added (s/one LeafletGeoJson "leafletGeoJson")))
 
 (defn validate
   "Middleware which augments underlying handler call with Schema validation.
@@ -58,6 +81,14 @@
   [geojson [_ value] :- GeoJSONReceivedEvent]
   value)
 
+(s/defn region-data-received-handler
+  [db [_ region data] :- RegionDataReceivedEvent]
+  (assoc-in db [:regions-data region] data))
+
+(s/defn leaflet-geojson-added-handler
+  [db [_ map] :- LeafletGeoJsonAddedEvent]
+  (assoc db :leaflet-geojson map))
+
 (register-handler
  :initialize
  [validate]
@@ -68,34 +99,92 @@
  [(path [:geojson]) validate]
  geojson-received-handler)
 
+(register-handler
+ :region-data-received
+ [validate]
+ region-data-received-handler)
 
-(defn map-holder []
-  [:div [:h3 "Map"]
-   [:div#map]])
+(register-handler
+ :leaflet-geojson-added
+ [validate]
+ leaflet-geojson-added-handler)
 
-(defn add-map-with [geojson]
+
+(defn map-holder [regions-data-ratom]
   (fn []
-    (let [map (.setView (.map js/L "map") #js [49 31] 5)]
+    ;; Hack to run component-did-update on regions-data-ratom change
+    @regions-data-ratom
+    [:div [:h3 "Map"]
+     [:div#map]]))
+
+(defn get-color [wins treasons]
+  (let [rating (- wins treasons)]
+    (cond
+      (< rating 0) "#fee090"
+      (< rating -10) "#fdae61"
+      (< rating -20) "#f46d43"
+      (< rating -30) "#d73027"
+      (< rating -40) "#a50026"
+
+      (> rating 0) "#e0f3f8"
+      (> rating 10) "#abd9e9"
+      (> rating 20) "#74add1"
+      (> rating 30) "#4575b4"
+      (> rating 40) "#313695"
+
+      :else "#ffffbf")))
+
+(defn style [regions-data]
+  (fn [feature]
+    {:fillColor (let [region (-> feature
+                                  (.-properties)
+                                  (.-name))
+                      region-data (get regions-data region {})]
+                  (get-color (get region-data :wins 0)
+                             (get region-data :treasons 0)))
+     :weight 2
+     :opacity 1
+     :color "white"
+     :dashArray "3"
+     :fillOpacity 0.7}))
+
+(defn update-geojson-style [regions-data-ratom leaflet-geojson-ratom]
+  (fn []
+    (.eachLayer @leaflet-geojson-ratom
+                (fn [l]
+                  (->> ((style @regions-data-ratom) (.-feature l))
+                       clj->js
+                       (.setStyle l))))))
+
+(defn add-map-with [geojson regions-data]
+  (fn []
+    (let [map (.setView (.map js/L "map") #js [49 31] 5)
+          leaflet-geojson
+          (.geoJson js/L geojson
+                    (clj->js {:style (comp clj->js (style regions-data))}))]
+      (dispatch [:leaflet-geojson-added leaflet-geojson])
       (.addTo (.tileLayer js/L "http://{s}.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}"
-                          (clj->js {:attribution "Map data &copy; [...]"
-                                    :id "gsnewmark.lp1opmko"
+                          (clj->js {:id "gsnewmark.lp1opmko"
                                     :token "pk.eyJ1IjoiZ3NuZXdtYXJrIiwiYSI6IkFfcXR6Q3cifQ.-8u0leDtUFLwPkO9kQRcTQ"
                                     :minZoom 5
                                     :maxZoom 6}))
               map)
-      (.addTo (.geoJson js/L geojson)
-              map))))
+      (.addTo leaflet-geojson map))))
 
 (defn map-component [geojson]
-  (reagent/create-class {:reagent-render map-holder
-                         :component-did-mount (add-map-with geojson)}))
+  (let [regions-data (subscribe [:regions-data])
+        leaflet-geojson (subscribe [:leaflet-geojson])]
+    (reagent/create-class
+     {:reagent-render (map-holder regions-data)
+      :component-did-mount (add-map-with geojson @regions-data)
+      :component-did-update (update-geojson-style regions-data leaflet-geojson)})))
 
 (defn page-component []
   (let [geojson (subscribe [:geojson])]
     (fn []
       [:div
        [:h2 "loki"]
-       (if @geojson
+       (if (and @geojson)
          [map-component @geojson]
          "Loading...")])))
 
